7890315f7cd0512fab95cdbc522cb0f26933b4fb
kevin lima <kevin.lima.code@gmail.com>
2018-01-31 23:21:58 +0000

    Made the text appear on the website

--- js/script.js
+++ js/script.js
@@ -17,30 +17,32 @@
 
 // Songs
 var mySong = {
-	"title":"24K Magic",
-	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
-}
+	"title": "Feel Good inc",
+	"artist": "Gorillaz",
+	"mp3-url": "https://open.spotify.com/artist/3AA28KZvwAUcZuOKwyblJQ",
+	"image-url": "https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg", //fix the img//
+};
 
-var myPlayList = [
-	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+
+
+
+var myPlayList = [{
+		"title": "24K Magic",
+		"artist": "Bruno Mars",
+		"mp3-url": "https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+		"image-url": "https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 	},
 	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"title": "Sir Duke",
+		"artist": "Stevie Wonder",
+		"mp3-url": "https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
+		"image-url": "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
 	},
 	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"title": "Sorry",
+		"artist": "Justin Bieber",
+		"mp3-url": "https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+		"image-url": "http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
 	}
 
 ]
@@ -48,27 +50,29 @@ var myPlayList = [
 
 
 // DOCUMENT READY FUNCTION
-$( document ).ready(function() {
-  
+$(document).ready(function() {
+	$('body').append("<p> : " + mySong.+ "</p>");
+	$('body').append("<p> My Song: " + mySong.title + "</p>");
+	$('body').append("<p> Artist: " + mySong.artist + "</p>");
 
 
 
 });
 
-function displayList(){
+function displayList() {
 
 
-  
+
 }
 
-function clearList(){
-  
-  
-  
+function clearList() {
+
+
+
 }
 
-function addSong(){
- 
-  
-  
+function addSong() {
+
+
+
 }
